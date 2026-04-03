40251bedf39f20bba049c7b2f96f604aae02f1c2
AnubhabDey-TransEV <anubhabdey@tgplin.com>
2026-04-03 11:49:54 +0530

    Added an API for returning booking details

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from app.db.database import engine
 from app.driver import driver_kyc, vehicle
 from app.driver.driverprofile import router as driverprofile_router
 from app.driver.driverprofileshow import router as driverprofileshow_router
-from app.driver.trips import booking_details, route_trip_details, trip_details
+from app.driver.trips import booking_details_service, route_trip_details, trip_details
 from app.driver.trips.routes import router as driver_routes_router
 from app.driver.trips.scheduled_trip import router as scheduled_trip_router
 from app.jobs.payment_reconciler import payment_reconcile_loop
