8b7ebf6c31810532b2a02375b51cf4a79481c6d4
AnubhabDey-TransEV <anubhabdey@tgplin.com>
2026-04-17 15:33:57 +0530

    Changed method name

--- app/admin/logic/service.py
+++ app/admin/logic/service.py
@@ -837,7 +837,7 @@ class AdminService:
 			boarded_stmt = (
 				select(
 					schema.TripBooking.id.label("booking_id"),
-					schema.User.PassengerProfile.full_name.label("user_name"),
+					schema.User.passenger_profile.full_name.label("user_name"),
 				)
 				.join(
 					schema.User,
